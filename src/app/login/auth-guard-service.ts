ccdd30dfa436a78a493ca8d04a7db5169f606c43
NGDK78 <karol.koson@motorolasolutions.com>
2021-09-07 22:26:05 +0200

    fixexd code smells

--- src/app/login/auth-guard-service.ts
+++ src/app/login/auth-guard-service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
 import {Observable} from "rxjs";
 import {SocialAuthService, SocialUser} from "angularx-social-login";
-import {map, tap} from "rxjs/operators";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
